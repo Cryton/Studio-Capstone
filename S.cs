671f5c9a27caf0fd130ac6839411998b072f8d7b
Lawlzaur <sday2@spsu.edu>
2014-01-19 17:00:25 -0500

    My Codes

---
+++ S.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S : MonoBehaviour {
+	public Transform target;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float ang = Vector3.Angle(target.position-transform.position,-transform.right);
+		float ang2 = Vector3.Angle(target.position-transform.position,transform.right);
+		float ang3 = Vector3.Angle(target.position-transform.position,transform.forward);
+		float vertAng = Vector3.Angle(target.position-transform.position,transform.up);
+		print(vertAng);
+		if(ang3 > 2)
+		{
+			if(ang < ang2)
+			{
+				transform.Rotate(-transform.up);
+			}
+			else
+			{
+				transform.Rotate(transform.up);
+			}
+			
+		}
+		if(vertAng > 2)
+		{
+			
+		}
+		
+	}
+}
---
+++ S_Mech.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Mech : MonoBehaviour {
+	public GameObject torso,legs;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			legs.transform.Rotate(Vector3.down);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			legs.transform.Rotate(Vector3.up);
+		}
+		torso.transform.Rotate(Vector3.up*Input.GetAxis("Mouse X"),Space.World);
+		torso.transform.Rotate(Vector3.left*Input.GetAxis("Mouse Y"),Space.Self);
+		//torso.transform.rotation.eulerAngles += new Vector3(Input.GetAxis("Mouse Y"),0,0);
+		
+	}
+}
