f39d5e7c0d06e526667c41b82c0935b6a7152887
Cryton <aford3@spsu.edu>
2014-02-03 19:32:58 -0500

    nonsense

---
+++ Assets/Scripts/FPS/FpsGooey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FpsGooey : MonoBehaviour {
+
+	Rect mapR, 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		//GUI.Box (new Rect
+	}
+}
